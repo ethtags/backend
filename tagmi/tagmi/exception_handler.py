d9bed6229a6f629baca7e91377a971c5bed56c6c
nullbitx8 <nullbitx8@protonmail.com>
2022-05-12 21:59:50 +0100

    appease linter

--- tagmi/tagmi/exception_handler.py
+++ tagmi/tagmi/exception_handler.py
@@ -1,12 +1,12 @@
 """
 https://gist.github.com/twidi/9d55486c36b6a51bdcb05ce3a763e79f
 
-Sometimes in your Django model you want to raise a ``ValidationError`` in the ``save`` method, for
-some reason.
-This exception is not managed by Django Rest Framework because it occurs after its validation 
-process. So at the end, you'll have a 500.
-Correcting this is as simple as overriding the exception handler, by converting the Django
-``ValidationError`` to a DRF one.
+Sometimes in your Django model you want to raise a ``ValidationError``
+in the ``save`` method, for some reason.
+This exception is not managed by Django Rest Framework because it
+occurs after its validation process. So at the end, you'll have a 500.
+Correcting this is as simple as overriding the exception handler, by
+converting the Django ``ValidationError`` to a DRF one.
 """
 
 from django.core.exceptions import ValidationError as DjangoValidationError
@@ -20,7 +20,7 @@ def exception_handler(exc, context):
     Must be set in settings:
     >>> REST_FRAMEWORK = {
     ...     # ...
-    ...     'EXCEPTION_HANDLER': 'mtp.apps.common.drf.exception_handler',
+    ...     'EXCEPTION_HANDLER': 'path.to.the.exception_handler',
     ...     # ...
     ... }
     For the parameters, see ``exception_handler``
